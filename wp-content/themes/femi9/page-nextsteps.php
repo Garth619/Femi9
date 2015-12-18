1f7874a3bf4e53065aaa5fdce4cf5a4932065a7b
garrett cullen <info@garrettcullen.com>
2015-12-18 09:28:23 -0800

    push

---
+++ wp-content/themes/femi9/page-nextsteps.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Template Name: Next Steps
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+
+
+<div class="banner">
+	
+	<img src="<?php bloginfo('template_directory');?>/images/mobile-banner4.jpg"/>
+
+</div><!-- banner -->
+
+<div class="mobile_femi_bar">
+	<img src="<?php bloginfo('template_directory');?>/images/femi.png"/>
+	
+</div><!-- mobile_femi_bar -->
+
+<div class="content_area">
+	
+	<div class="sand"></div>
+	<div class="white_content_box page_three_box_one">
+		
+		<h1>Let’s Have a&nbsp;Conversation</h1>
+		
+		<p>We are looking for strategic partners who can commit to great customer experiences that prompt customers to come back to femi9. We have designed our proven formula for success, a formula that is working from market to market.  We know how to attract, bring in new customers and retain them.  Our robust systems and an expert and dedicated team provide all the tools for sustainable performance and growth. We are now looking for strategic partner franchisees worldwide who are committed to giving our customers affordable quality and feminine, timeless elegance in fashion, all complemented by a winning
+customer experience.</p>
+
+		
+	</div><!-- white_content_box -->
+	
+
+	<div class="mobile_models7_wrapper">
+		
+			<img class="palm_trees" src="<?php bloginfo('template_directory');?>/images/palm-trees.jpg"/>
+			<img class="mobile_models7" src="<?php bloginfo('template_directory');?>/images/mobile-model7.jpg"/>
+			<div class="orange2"></div>
+		</div>
+		
+		
+		<div class="white_content_box page_three_box_two">
+		
+		<h1>Let’s Get&nbsp;Started</h1>
+		
+<p>form</p>		 
+
+		
+	</div><!-- white_content_box -->
+	
+	
+	<div class="mobile_models8_wrapper">
+		
+			
+			<img class="mobile_models8" src="<?php bloginfo('template_directory');?>/images/mobile-model8.jpg"/>
+			<img class="rose2" src="<?php bloginfo('template_directory');?>/images/rose2.jpg"/>
+		</div>
+		
+		
+		
+	
+		
+	
+		
+<!-- 		<div class="wavy"></div> -->
+		
+	
+</div><!-- content_area -->
+
+
+
+
+
+<?php get_footer(); ?>
