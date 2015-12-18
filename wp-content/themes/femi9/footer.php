f640fb4b323e7cdbfcccfc4b263abdd08918758b
garrett cullen <info@garrettcullen.com>
2015-12-17 17:34:06 -0800

    push

--- wp-content/themes/femi9/footer.php
+++ wp-content/themes/femi9/footer.php
@@ -1,51 +1,19 @@
 <?php
 /**
- * Template for displaying the footer
+ * footer
  *
- * Contains the closing of the id=main div and all content
- * after. Calls sidebar-footer.php for bottom widgets.
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
+
  */
 ?>
-	</div><!-- #main -->
 
-	<div id="footer" role="contentinfo">
-		<div id="colophon">
 
-<?php
-	/*
-	 * A sidebar in the footer? Yep. You can can customize
-	 * your footer with four columns of widgets.
-	 */
-	get_sidebar( 'footer' );
-?>
+</div><!-- wrapper -->
 
-			<div id="site-info">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-					<?php bloginfo( 'name' ); ?>
-				</a>
-			</div><!-- #site-info -->
+<div class="fixed_footer">
+	<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
+</div><!-- mobile footer -->
 
-			<div id="site-generator">
-				<?php
-				/**
-				 * Fires before the Twenty Ten credits in the footer.
-				 *
-				 * @since Twenty Ten 1.0
-				 */
-				do_action( 'twentyten_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyten' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyten' ); ?>"><?php printf( __( 'Proudly powered by %s.', 'twentyten' ), 'WordPress' ); ?></a>
-			</div><!-- #site-generator -->
-
-		</div><!-- #colophon -->
-	</div><!-- #footer -->
-
-</div><!-- #wrapper -->
-
-<?php
+	<?php
 	/*
 	 * Always have wp_footer() just before the closing </body>
 	 * tag of your theme, or you will break many plugins, which
---
+++ wp-content/themes/femi9/page-welcome.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template Name: Welcome
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+
+
+<div class="banner">
+	
+	<img src="<?php bloginfo('template_directory');?>/images/mobile-banner1.jpg"/>
+
+</div><!-- banner -->
+
+<div class="mobile_femi_bar">
+	<img src="<?php bloginfo('template_directory');?>/images/femi.png"/>
+	
+</div><!-- mobile_femi_bar -->
+
+
+
+
+
+<?php get_footer(); ?>
