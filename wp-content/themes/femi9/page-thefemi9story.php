dc180a4b4c1dce2c25cd1ace140ab37079cb4e55
garrett cullen <info@garrettcullen.com>
2015-12-17 21:14:01 -0800

    Push

---
+++ wp-content/themes/femi9/page-thefemi9story.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Template Name: The Femi9 Story
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+
+
+<div class="banner">
+	
+	<img src="<?php bloginfo('template_directory');?>/images/mobile-banner2.jpg"/>
+
+</div><!-- banner -->
+
+<div class="mobile_femi_bar">
+	<img src="<?php bloginfo('template_directory');?>/images/femi.png"/>
+	
+</div><!-- mobile_femi_bar -->
+
+<div class="content_area">
+	
+	<div class="rose"></div><!-- rose -->
+	<div class="white_content_box page_two_box_one">
+		
+		<h1>THE FEMI9 BUSINESS&nbsp;MODEL</h1>
+		
+		<p>Our business model spans the end-to-end process that begins with a unique design inspiration and results in our customers experiencing and selecting from our latest collections.  This process is made up of a number of steps collaboratively brought to life by our talented and committed team globally:</p>
+
+<ul>
+<li>Our experienced and expert design team investigates the latest trends across the world’s fashion capitals and translates these designs into seasonal collections that, above all, reflect femi9's commitment to femininity, elegance and timelessness.</li>
+
+<li>Our dedicated team in China oversees the production of samples and final collections, always committed to the highest standards of quality assurance.</li>
+
+<li>Once the season’s orders are submitted and processed, our approved and expert manufacturing partners produce these collections to the highest standards and manufacturing specifications. We are committed to making sure that femi9 always remains a trusted brand for both style and quality among our discerning customers.</li>
+</ul>
+		
+	</div><!-- white_content_box -->
+	
+
+	
+	
+	<div class="mission_statement">
+		<h1 class="mission_header">FEMI9 Vision</h1>
+		<p>To reach the world with our feminine fashion experience</p>
+		<h1 class="mission_header">FEMI9 Mission</h1>
+		<p>Committed and inspired to make every day a special occasion through elegant and affordable fashion</p>
+	</div><!-- mission_statement -->
+	
+	<div class="mobile_models2_wrapper">
+		<div class="orange_models2"></div><!-- orange_models1 -->
+			
+			<img class="mobile_models2" src="<?php bloginfo('template_directory');?>/images/mobile-models2.jpg"/>
+			<img class="bwcurtain" src="<?php bloginfo('template_directory');?>/images/bw-curtain.jpg"/>
+		</div><!-- mobile_models1_wrapper -->
+		
+		
+		<div class="white_content_box page_two_box_two">
+		
+		<h1>THE FEMI9 BUSINESS&nbsp;MODEL</h1>
+		
+		 
+
+<ul>
+<li>Each order is prepared, packed and ready to ship around the world.</li>
+
+<li>Simultaneously, instructions on merchandising and window display design are communicated to all our stores, to ensure a 	  consistent seasonal look and feel across the entire femi9 store network.</li>
+
+<li>Our season-specific marketing campaign is developed to reflect the collections’ themes and the femi9 brand identity.  	   This includes our highly anticipated seasonal catalogs, featuring our latest stunning destination shoots for each 		   major collection, as well as our engaging digital and social media campaigns- all custom designed and launched to 		   support each new collection.</li>
+
+<li>Finally, our signature customer experience at the store comes to life. Supported through our ongoing training & 			   development initiatives, our store team engages and consults with customers, inspiring and helping them to piece together 	   a range of fashion possibilities.</li>
+</ul>
+		
+	</div><!-- white_content_box -->
+		
+		<div class="orange_curtains"></div><!-- orange_curtains -->
+		
+	
+</div><!-- content_area -->
+
+
+
+
+
+<?php get_footer(); ?>
--- wp-content/themes/femi9/page-welcome.php
+++ wp-content/themes/femi9/page-welcome.php
@@ -26,6 +26,30 @@ get_header(); ?>
 	
 </div><!-- mobile_femi_bar -->
 
+<div class="content_area">
+	
+	<div class="flower"></div><!-- rose -->
+	<div class="white_content_box page_one_box_one">
+		
+		<h1>femi9: A Celebration of&nbsp;Femininity</h1>
+		
+		<p>Welcome to femi9!  Born of an Arabian heritage and a growing fashion legacy, femi9 embodies a celebration of femininity, elegance and affordability.  With collections created and brought to life season after season by a culturally diverse design team, the femi9 style appeals to women from all walks of life.  Our collections draw out the femininity of all women, remaining true to our heritage and meeting our customers’ needs and expectations as part of our Customer-Centric environment and brand.  In our vision to share our elegant, affordable and sought-after collections with the rest of world, we are seeking strategic partners with similar values to ours and to jointly expand our thriving business internationally.</p>
+
+<p>Here you will learn more about femi9, our goals, what we value and what it takes to qualify to become our franchisee and strategic partner in growing our brand across new markets.</p> 
+		
+	</div><!-- white_content_box -->
+	
+	<div class="pink_curtains"></div><!-- pink_curtains -->
+	
+	<div class="mobile_models1_wrapper">
+		<div class="orange_models1"></div><!-- orange_models1 -->
+		<img class="mobile_grid" src="<?php bloginfo('template_directory');?>/images/grid.png"/>
+		<img class="mobile_models1" src="<?php bloginfo('template_directory');?>/images/mobile-models1.jpg"/>
+		<img class="um" src="<?php bloginfo('template_directory');?>/images/um.jpg"/>
+	</div><!-- mobile_models1_wrapper -->
+	
+</div><!-- content_area -->
+
 
 
 
